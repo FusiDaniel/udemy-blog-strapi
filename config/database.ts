aa4bc33cc843f6272cd0f6cffbcee6ee781a630d
FusiDaniel <fpires.daniel@gmail.com>
2022-12-26 18:31:27 -0300

    changed pg variables

--- config/database.ts
+++ config/database.ts
@@ -2,11 +2,11 @@ export default ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '0.0.0.0'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'blog-strapi'),
+      host: env('DATABASE_HOST', 'containers-us-west-137.railway.app'),
+      port: env.int('DATABASE_PORT', 6341),
+      database: env('DATABASE_NAME', 'railway'),
       user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'senha'),
+      password: env('DATABASE_PASSWORD', 'kKxcVcp1B1iX89dZfG5q'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
